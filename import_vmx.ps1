ec6924040e13e077c52f0785d8a34d7eacaddd53
JJ Vidañez <jj.vidanez@esl-asia.com>
2020-11-27 09:55:57 +0100

    fixing quote symbols

--- import_vmx.ps1
+++ import_vmx.ps1
@@ -4,21 +4,21 @@ connect-viserver -server ENTER VCENTER FQDN HERE
 #####################################################################
  
 # Variables: Update these to the match the environment
-$Cluster = &quot;ENTER CLUSTER NAME HERE&quot;
-$Datastores = get-datastorecluster &quot;ENTER DATASTORE CLUSTER NAME HERE&quot; | get-datastore
-$VMFolder = &quot;ENTER FOLDER NAME HERE&quot;
+$Cluster = "ENTER CLUSTER NAME HERE"
+$Datastores = get-datastorecluster "ENTER DATASTORE CLUSTER NAME HERE" | get-datastore
+$VMFolder = "ENTER FOLDER NAME HERE"
 $ESXHost = Get-Cluster $Cluster | Get-VMHost | select -First 1
  
 foreach($Datastores in $Datastores) {
 # Set up Search for .VMX Files in Datastore Cluster
 $ds = Get-Datastore -Name $Datastores | %{Get-View $_.Id}
 $SearchSpec = New-Object VMware.Vim.HostDatastoreBrowserSearchSpec
-$SearchSpec.matchpattern = &quot;*.vmx&quot;
+$SearchSpec.matchpattern = "*.vmx"
 $dsBrowser = Get-View $ds.browser
-$DatastorePath = &quot;[&quot; + $ds.Summary.Name + &quot;]&quot;
+$DatastorePath = "[" + $ds.Summary.Name + "]"
  
 # Find all .VMX file paths in Datastore variable and filters out .snapshot
-$SearchResult = $dsBrowser.SearchDatastoreSubFolders($DatastorePath, $SearchSpec) | where {$_.FolderPath -notmatch &quot;.snapshot&quot;} | %{$_.FolderPath + ($_.File | select Path).Path}
+$SearchResult = $dsBrowser.SearchDatastoreSubFolders($DatastorePath, $SearchSpec) | where {$_.FolderPath -notmatch ".snapshot"} | %{$_.FolderPath + ($_.File | select Path).Path}
  
 # Register all .VMX files with vCenter
 foreach($VMXFile in $SearchResult) {
